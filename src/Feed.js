15dbb693a7e69acf4deb805f1a52b1f87eb53526
David Ordone <david9108@hotmail.com>
2024-07-03 14:49:24 -0300

    finalizando tela login

---
+++ src/Feed.js
@@ -0,0 +1,10 @@
+function Feed() {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            
+        </div>
+    );
+  }
+  
+  export default Feed;
+  
